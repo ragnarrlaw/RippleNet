fb32f42cbb1e9d2aabc0a4346f711d9a0f0a6d53
ragnarrlaw <carmichealclair@yahoo.com>
2025-12-30 15:42:16 +0530

    add basic structure for v0.0.0 - remove the thread handling code from the basic version - remove retain and release

--- src/ct_tensor.c
+++ src/ct_tensor.c
@@ -1,25 +1,30 @@
 /* version v0.0.0 ct_tensor */
-
 #include "ct.h"
 
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef uint32_t ct_flags_t;
+
+#define CT_FLAG_OWNS_DATA (1u << 0)
+#define CT_FLAG_BORROWED (1u << 1)
+#define CT_FLAG_CONTIGUOUS (1u << 2)
+#define CT_FLAG_READONLY (1u << 3)
+#define CT_FLAG_CUSTOM_FREE (1u << 4)
+
 struct ct_tensor {
   void *data;
   ct_dtype_t dtype;
+  int32_t num_dim;
+  int64_t *shape;
+  int64_t *stride;
+  ct_flags_t flags;
 };
 
 /* create tensor */
 ct_tensor_t *ct_tensor_create(ct_dtype_t dtype, const ct_shape_t *shape) {
   return NULL;
 }
-
-/* borrow a tensor never owned */
-void ct_tensor_retain(ct_tensor_t *t) {}
-
-/* release a tensor */
-void ct_tensor_release(ct_tensor_t *t) {}
 
 /* return pointer to tensor data */
 void *ct_tensor_data(ct_tensor_t *t);
