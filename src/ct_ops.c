cd1fd9c410c34662cc097abf90ff06eb09018194
ragnarrlaw <carmichealclair@yahoo.com>
2025-12-30 15:42:50 +0530

    add basic skeleton structure for tensor operations add and multiply

---
+++ src/ct_ops.c
@@ -0,0 +1,10 @@
+#include "ct.h"
+
+ct_status_t ct_add(const ct_tensor_t *a, ct_tensor_t *b, ct_tensor_t **out) {
+  return 0;
+}
+
+ct_status_t ct_mat_mul(const ct_tensor_t *a, ct_tensor_t *b,
+                       ct_tensor_t **out) {
+  return 0;
+}
