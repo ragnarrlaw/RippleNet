df8fa72a4facdb696a406361dabdaa3bbf95e21f
ragnarrlaw <carmichealclair@yahoo.com>
2026-01-03 22:05:53 +0530

    update library api - use enums for errors and status

--- include/ct.h
+++ include/ct.h
@@ -1,49 +1,50 @@
-/* public api for ct v0.0.0 -> single thread version */
 #ifndef CT_H
 #define CT_H
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#include <stdatomic.h>
 #include <stdint.h>
 
-// standard error codes & definitions
-typedef int32_t ct_status_t;
-#define CT_OK 0
-#define CT_ERR_INVALID_ARG 1
-#define CT_ERR_SHAPE 2
-#define CT_ERR_DTYPE 3
-#define CT_ERR_NOMEM 4
-#define CT_ERR_INTERNAL 5
+typedef enum ct_status {
+  CT_STATUS_SUCCESS = 0,
+  CT_STATUS_ERROR_INVALID_ARGUMENT,
+  CT_STATUS_ERROR_SHAPE_MISTMATCH,
+  CT_STATUS_ERROR_DTYPE_INVALID,
+  CT_STATUS_ERROR_DTYPE_INCOMPATIBLE,
+  CT_STATUS_ERROR_NO_MEMORY,
+  CT_STATUS_ERROR_INTERNAL,
+} ct_status_t;
 
-// data types -> wuth #bytes
-typedef int32_t ct_dtype_t;
-#define CT_F32 4
-#define CT_F64 8
-#define CT_I32 4
+/* INITIAL VERSIONS ONLY SUPPORT FLOAT 32 */
+typedef enum ct_dtype {
+  CT_DTYPE_FLOAT32 = 0,
+  CT_DTYPE_FLOAT64,
+  CT_DTYPE_INTEGER32,
+} ct_dtype_t;
+
+#define MAX_DIMS 8
 
 // main structs
 typedef struct ct_tensor ct_tensor_t;
+typedef struct ct_storage ct_storage_t;
 
 typedef struct {
   int32_t ndim;
-  int64_t *shape;
-  int64_t *stride;
+  int64_t shape[MAX_DIMS];
 } ct_shape_t;
 
-/* create tensor */
 ct_tensor_t *ct_tensor_create(ct_dtype_t dtype, const ct_shape_t *shape);
-/* return tensor data type */
 ct_dtype_t ct_tensor_dtype(const ct_tensor_t *t);
-/* return tensor shape */
 const ct_shape_t *ct_tensor_shape(const ct_tensor_t *t);
-/* return pointer to tensor data */
-void *ct_tensor_data(ct_tensor_t *t);
+ct_storage_t *ct_tensor_data(ct_tensor_t *t);
 
 ct_status_t ct_add(const ct_tensor_t *a, ct_tensor_t *b, ct_tensor_t **out);
 ct_status_t ct_mat_mul(const ct_tensor_t *a, ct_tensor_t *b, ct_tensor_t **out);
+ct_tensor_t *ct_transpose(const ct_tensor_t *t);
+ct_status_t ct_reshape(const ct_tensor_t *t, const ct_shape_t *ns);
 
-// ct_tensor ops (lazy)
 #ifdef __cplusplus
 }
 #endif // __cplusplus
